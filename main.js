62ca4b5e420e4f14be624d3c6312ea893995624a
lAlejandrocd <lalejandrocd1@gmail.com>
2024-12-16 22:40:39 -0500

    implementando doom html

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-import './src/index.js';
 
 import './style.css';
+import { App } from './src/index.js';
+
+
+
+//Acá vamos a invocar el app para que haga el render del html a la página
+App('#app');
--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
 // Defaults to weight 400
 import '@fontsource/ia-writer-mono';
+
+/**
+ * nombreArchivo.html?raw
+ * ? = lo usamos para identificar el archivo en raw, es propio de vite
+ * raw = significa que es en crudo, es decir, texto plano
+ */
+import container from './app/container.html?raw';
+import footer from './app/footer.html?raw';
+
+/**
+ * @param {String} elementId //Recibe el id del contenedor en donde quiero renderizar el html
+ */
+export const App = (elementId) => {
+
+  (() => {
+
+    //Creo el div
+    const app = document.createElement('div');
+
+    //Inserto el html en crudo en el div
+    app.innerHTML = container;
+
+    //Del párametro que envié desde main.js será el lugar en donde voy a renderizar el html.
+    document.querySelector(elementId).append(app);
+
+  })();
+}
