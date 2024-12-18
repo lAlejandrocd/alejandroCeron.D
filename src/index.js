86a6cba193490784c51949138ead97469e2111d4
lAlejandrocd <lalejandrocd1@gmail.com>
2024-12-17 20:04:13 -0500

    implementado html? y css desde import

--- src/index.js
+++ src/index.js
@@ -5,9 +5,8 @@ import '@fontsource/ia-writer-mono';
  * nombreArchivo.html?raw
  * ? = lo usamos para identificar el archivo en raw, es propio de vite
  * raw = significa que es en crudo, es decir, texto plano
- */
-import container from './app/container.html?raw';
-import footer from './app/footer.html?raw';
+*/
+import html from './app/html.html?raw';
 
 /**
  * @param {String} elementId //Recibe el id del contenedor en donde quiero renderizar el html
@@ -19,8 +18,10 @@ export const App = (elementId) => {
     //Creo el div
     const app = document.createElement('div');
 
+    app.setAttribute('class', 'page1');
+
     //Inserto el html en crudo en el div
-    app.innerHTML = container;
+    app.innerHTML = html;
 
     //Del párametro que envié desde main.js será el lugar en donde voy a renderizar el html.
     document.querySelector(elementId).append(app);
