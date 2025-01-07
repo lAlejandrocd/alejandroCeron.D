914b81a31109ddbdc54f091ab61a6111acd1d1ff
lAlejandrocd <lalejandrocd1@gmail.com>
2025-01-06 19:02:47 -0500

    cambio en rutas

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,4 @@ import { defineConfig } from "vite";
 export default defineConfig({
   // link de la página web, no del repositorio.
   base:'https://lalejandrocd.github.io/alejandroCeron.D/'
-  // base:'https://lAlejandrocd.github.io/alejandroCeron.D'
 })
